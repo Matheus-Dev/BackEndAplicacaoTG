290a68a816abfc5d63c3c2daf0ad16a64bb81ed6
MatheusAugustoDev <matheusaugusto.dev@gmail.com>
2017-12-06 09:05:44 -0200

    Removendo API KEY SendGrid

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ global.SALT_KEY = '54D83741-6E01-4447-A19E-4C55E25F7083';
 global.EMAIL_TMPL = 'Sua Chave de Registro: <strong>{0}</strong>.';
 
 module.exports = {
-    connectionString: 'mongodb://admin:harasadmin@ds042607.mlab.com:42607/haraswebdb',
-    sendgridKey: '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'
+    connectionString: 'mongodb://admin:harasadmin@ds042607.mlab.com:42607/haraswebdb'
     //connectionString: 'mongodb://localhost/aplicacaotg'
 }
