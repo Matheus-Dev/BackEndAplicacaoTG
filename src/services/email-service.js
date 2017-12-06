3acff50ba8427136ad617946edc1fac81689cd80
MatheusAugustoDev <matheusaugusto.dev@gmail.com>
2017-12-06 09:48:36 -0200

    Ajustando Envio de Email

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var config = require('../config');
+/*var config = require('../config');
 var sendgrid = require('sendgrid')(process.env.SENDGRID_APIKEY);
 
 exports.send = async (to, subject, body) => {
@@ -12,5 +12,22 @@ exports.send = async (to, subject, body) => {
 		html: body
 
 	});
+
+}*/
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.send = async (to, subject, body) => {
+
+	msg = {
+  		to: to,
+  		from: 'matheusaugusto.dev@gmail.com',
+  		subject: subject,
+  		text: 'Chave de Registro - HarasMobile',
+  		html: body
+	};
+
+	sgMail.send(msg);
 
 }
